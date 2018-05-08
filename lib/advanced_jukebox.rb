2042e325770bee9875d1fc55ab76c06165bf127c
Steven Chen <ysteven.chen2@gmail.com>
2018-05-08 17:35:20 +0000

    automatically backed up by learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -3,13 +3,13 @@
 #with the correct path to this directory on your computer
 
 my_songs = {
-"Go Go GO" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/01.mp3',
-"LiberTeens" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/02.mp3',
-"Hamburg" =>  '< path to this directory >/jukebox-cli/audio/Emerald-Park/03.mp3',
-"Guiding Light" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/04.mp3',
-"Wolf" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/05.mp3',
-"Blue" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/06.mp3',
-"Graduation Failed" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/07.mp3'
+"Go Go GO" => '/Users/weistevenchen/.atom/.learn-ide/home/steven0608/jukebox-cli/audio/Emerald-Park/01.mp3',
+"LiberTeens" => '/Users/weistevenchen/.atom/.learn-ide/home/steven0608/jukebox-cli/audio/Emerald-Park/02.mp3',
+"Hamburg" =>  '< /Users/weistevenchen/.atom/.learn-ide/home/steven0608/jukebox-cli/audio/Emerald-Park/03.mp3',
+"Guiding Light" => '/Users/weistevenchen/.atom/.learn-ide/home/steven0608/jukebox-cli/audio/Emerald-Park/04.mp3',
+"Wolf" => '/Users/weistevenchen/.atom/.learn-ide/home/steven0608/jukebox-cli/audio/Emerald-Park/05.mp3',
+"Blue" => '</Users/weistevenchen/.atom/.learn-ide/home/steven0608/jukebox-cli/audio/Emerald-Park/06.mp3',
+"Graduation Failed" => '/Users/weistevenchen/.atom/.learn-ide/home/steven0608/jukebox-cli/audio/Emerald-Park/07.mp3'
 }
 
 def help
