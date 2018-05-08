c0f9692045f31073519e17cf3f19a0bd916d54a7
Steven Chen <ysteven.chen2@gmail.com>
2018-05-08 16:55:34 +0000

    automatically backed up by learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -14,7 +14,11 @@
 
 def help
   #this method should be the same as in jukebox.rb
-
+puts "I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program"
 end
 
 
